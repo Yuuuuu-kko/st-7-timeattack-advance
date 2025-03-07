3d94ceaa3734be53a8c64ef7c4744eec0268c826
KyoungMIn YOO <91euriel@gmail.com>
2025-03-07 12:47:51 +0900

    타임어택

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Detail from "../pages/Detail";
 import Home from "../pages/Home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const qurryClient = new QueryClient();
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
+      <QueryClientProvider client={QueryClient}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
